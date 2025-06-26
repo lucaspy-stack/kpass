256702f999b9502dfc442f71feca985c32a0c81a
lucas-stack <lucas.workps@gmail.com>
2025-06-26 15:57:40 -0300

    Update Usage_Suggestion.py

--- Usage_Suggestion.py
+++ Usage_Suggestion.py
@@ -2,11 +2,11 @@
 
 from Password_Generator import pass_generator, save_to_txt
 
-name = input("Enter your full name: ")
+full_name = input("Enter your full name: ")
 age = input("Enter your age: ")
 data = input("Enter your date of birth (dd/mm/yyyy): ")
 
-passwords = pass_generator(name, age, data)
+passwords = pass_generator(full_name, age, data)
 save_to_txt(passwords)
 
 print(f"{len(passwords)} Passwords generated and saved successfully!")

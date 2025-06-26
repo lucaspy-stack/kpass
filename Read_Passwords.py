7fe32cdeaf855fbc2c6710d72df9b5d32f387494
lucas-stack <lucas.workps@gmail.com>
2025-06-26 16:10:37 -0300

    Update Read_Passwords.py

--- Read_Passwords.py
+++ Read_Passwords.py
@@ -1,5 +1,5 @@
 # Just to see the passwords in your terminal
 
-with open("passwords_generator\\pass_generated.txt", "r", encoding="utf-8") as test:
+with open("passwords_generator\\Pass_Generated.txt", "r", encoding="utf-8") as test:
     for line in test:
         print(line.strip())

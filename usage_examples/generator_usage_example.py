5f2b60ae50a6447c1c23625068a0ef62b4a6dfa5
lucas-stack <lucas.workps@gmail.com>
2025-06-27 15:50:56 -0300

    Update and rename usage_suggestion.py to generator_usage_example.py

---
+++ usage_examples/generator_usage_example.py
@@ -0,0 +1,8 @@
+from kpass import generator
+
+# Generate hundreds of password combinations based on personal data
+generator(
+    name="Johnny Silverhand",
+    age="34",
+    birth_date="16/11/1988"
+)

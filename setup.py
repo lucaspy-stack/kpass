e3d1840f505890a7d74243d6618ff4a7608f4cfb
lucas-stack <lucas.workps@gmail.com>
2025-06-27 09:46:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Passrord_Generator",
-    version="0.1.0",
+    name="kpass",
+    version="0.1.1",
     author="Lucas Paulino Da Silva",
-    author_email="lucas.workps@gmailcom",
-    description="The **passwords_generator** is a simple module that, given a full name, age, and birth date, generates password combinations between 6 and 18 characters",
+    author_email="lucas.workps@gmail.com",
+    description="kpass is a simple password generator based on name, age and birthdate.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/lucaspy-stack/passwords_generator",
+    url="https://github.com/lucaspy-stack/kpass",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

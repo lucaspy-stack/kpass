992627257dea4461bf5d5d4f908cae64922aae86
lucas-stack <lucas.workps@gmail.com>
2025-06-27 09:42:20 -0300

    Update and rename Usage_Suggestion.py to usage_suggestion.py

--- usage_suggestion.py
+++ usage_suggestion.py
@@ -1,5 +1,5 @@
 # Just a suggestion for use
-from Password_Generator import pass_generator, save_to_txt
+from generator import generator
 from rich import print
 
 full_name = input("Enter your full name: ")
@@ -7,6 +7,5 @@ age = input("Enter your age: ")
 data = input("Enter your date of birth (dd/mm/yyyy): ")
 
 passwords = pass_generator(full_name, age, data)
-save_to_txt(passwords)
 
 print(f"[bold green]{len(passwords)}[/] [cyan]senhas geradas e salvas com sucesso![/]")

745209297ad512d64fc9eb80986415fdc1cd8eb2
lucas-stack <lucas.workps@gmail.com>
2025-06-27 15:51:48 -0300

    Delete usage_examples directory

--- usage_examples/generator_usage_example.py
+++
@@ -1,8 +0,0 @@
-from kpass import generator
-
-# Generate hundreds of password combinations based on personal data
-generator(
-    name="Johnny Silverhand",
-    age="34",
-    birth_date="16/11/1988"
-)
--- usage_examples/read_passwords.py
+++
@@ -1,7 +0,0 @@
-# Just to see the passwords in your terminal
-import time 
-
-with open("pass_generated.txt", "r", encoding="utf-8") as test:
-    for line in test:
-        print(line.strip())
-        time.sleep(0.1)
